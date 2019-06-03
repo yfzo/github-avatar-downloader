2ff55172e21590f5076889f08b625031ad10cae1
Yifei <yifeizhou.to@gmail.com>
2019-06-03 23:34:09 +0000

    added authorization and user-agent headers

--- download_avatars.js
+++ download_avatars.js
@@ -1,14 +1,26 @@
 var request = require('request');
-var fs = require('fs');
+var secrets = require('secrets');
+//var fs = require('fs');
+console.log('Welcome to the GitHub Avatar Downloader!');
 
-var owner = process.argv[2];
-var repo = process.argv[3];
+// var owner = process.argv[2];
+// var repo = process.argv[3];
 
 function getRepoContributors (repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': 'Token ' + secrets.GITHUB_TOKEN
+    }
+  };
 
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
 }
 
-getRepoContributors("jquery", "jquery", function(err, results) {
+getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Results:", result);
 });
