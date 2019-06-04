997d805f180b9fde145ff42240afe05a2fbd0b09
Yifei <yifeizhou.to@gmail.com>
2019-06-04 01:31:52 +0000

    added comments

--- download_avatars.js
+++ download_avatars.js
@@ -6,10 +6,8 @@ var owner = process.argv[2];
 var repo = process.argv[3];
 
 function getRepoContributors (repoOwner, repoName, cb) {
-  if (!repoOwner || !repoName) {
 
-  }
-
+  //terminates and logs error message if either owner or repo not supplied
   try{
     if (!repoOwner || !repoName) throw "Missing repository owner or name argument";
   }
@@ -17,6 +15,7 @@ function getRepoContributors (repoOwner, repoName, cb) {
     console.log(err);
   }
 
+  //sets options for request
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
@@ -25,6 +24,7 @@ function getRepoContributors (repoOwner, repoName, cb) {
     }
   };
 
+  //gets data and passes the array of contributor objs to cb
   request(options, function(err, res, body) {
     if (!err && res.statusCode == 200) {
       var data = JSON.parse(body);
@@ -34,6 +34,7 @@ function getRepoContributors (repoOwner, repoName, cb) {
   });
 }
 
+//downloads image from given url and saves to filePath
 function downloadImageByURL(url, filePath) {
   request.get(url)
          .on('error', function(err) {
@@ -44,6 +45,7 @@ function downloadImageByURL(url, filePath) {
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+//connects and passes data from request to downloadImageByURL
 getRepoContributors(owner, repo, function(err, result) {
   console.log("Errors:", err);
 
