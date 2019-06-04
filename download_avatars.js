9f08099a8e935a9d957ba733618f1b90d9120aca
Yifei <yifeizhou.to@gmail.com>
2019-06-04 01:22:35 +0000

    added error message for when there are missing arguments

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,22 @@
 var request = require('request');
 var secrets = require('./secrets');
 var fs = require('fs');
-console.log('Welcome to the GitHub Avatar Downloader!');
 
 var owner = process.argv[2];
 var repo = process.argv[3];
 
 function getRepoContributors (repoOwner, repoName, cb) {
+  if (!repoOwner || !repoName) {
+
+  }
+
+  try{
+    if (!repoOwner || !repoName) throw "Missing repository owner or name argument";
+  }
+  catch(err) {
+    console.log(err);
+  }
+
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
@@ -28,14 +38,11 @@ function downloadImageByURL(url, filePath) {
   request.get(url)
          .on('error', function(err) {
            throw err;
-         // })
-         // .on('response', function(response) {
-         //   console.log('Response Status Code: ', response.statusCode);
          })
          .pipe(fs.createWriteStream(filePath));
 }
 
-//downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg");
+console.log('Welcome to the GitHub Avatar Downloader!');
 
 getRepoContributors(owner, repo, function(err, result) {
   console.log("Errors:", err);
